3ef47444e129624f97f8ff03e803242a8d77b277
Jonathan Martindale <109117468+JonForrest@users.noreply.github.com>
2024-02-23 19:08:37 -0600

    fix pathing issue

--- index.js
+++ index.js
@@ -13,40 +13,40 @@ var toDoItems=[];
 var workItems=[];
 
 
-app.use(express.static(__dirname+"public"));
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
     pageinfo.accessCurrentPage="todo";
     pageinfo.listItems=toDoItems;
-    res.render(__dirname+"index.ejs", pageinfo);
+    res.render(__dirname+"/views/index.ejs", pageinfo);
 });
 
 app.get("/todo", (req, res) => {
     pageinfo.accessCurrentPage="todo";
     pageinfo.listItems=toDoItems;
-    res.render(__dirname+"index.ejs", pageinfo);
+    res.render(__dirname+"/views/index.ejs", pageinfo);
 });
 
 app.post("/todo", (req, res) => {
     pageinfo.accessCurrentPage="todo";
     pageinfo.listItems=toDoItems;
     toDoItems.push(req.body.textEntry);
-    res.render(__dirname+"index.ejs", pageinfo);
+    res.render(__dirname+"/views/index.ejs", pageinfo);
 });
 
 app.get("/work", (req, res) => {
     pageinfo.accessCurrentPage="work";
     pageinfo.listItems=workItems;
-    res.render(__dirname+"index.ejs", pageinfo);
+    res.render(__dirname+"/views/index.ejs", pageinfo);
 });
 
 app.post("/work", (req, res) => {
     pageinfo.accessCurrentPage="work";
     pageinfo.listItems=workItems;
     workItems.push(req.body.textEntry);
-    res.render(__dirname+"index.ejs", pageinfo);
+    res.render(__dirname+"/views/index.ejs", pageinfo);
 });
 
 app.listen(port, () => {
